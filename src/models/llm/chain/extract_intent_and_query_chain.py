ce3fbeae8785f091eef3a1be16e11cacc3fa4b33
devch1013 <devch1013@gmail.com>
2024-02-20 21:10:35 +0900

    change summary line breaks

--- src/models/llm/chain/extract_intent_and_query_chain.py
+++ src/models/llm/chain/extract_intent_and_query_chain.py
@@ -21,8 +21,8 @@ class ExtractIntentAndQueryChain(BaseChatChain):
                 system_prompt_template:str=SYSTEM,
                 user_prompt_template:str=USER,
                 response_format="json",
-                model='gpt-4-0125-preview',
-                # model='gpt-3.5-turbo-0125',
+                # model='gpt-4-0125-preview',
+                model='gpt-3.5-turbo-0125',
                 temperature=0.0,
                 top_p=0.0,
                 verbose=False,) -> None:
